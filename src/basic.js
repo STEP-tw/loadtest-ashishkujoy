d0d58f2f0a08c6046d71ae779a35d9ff0a94f7a1
Ashish Joy <kumarashishsheamus@gmail.com>
2018-08-11 11:59:09 +0530

    Add basic functions

--- src/basic.js
+++ src/basic.js
@@ -4,11 +4,15 @@
   based on whether the number is even or not
 */
 
+const isEven = (number) => number % 2 == 0;
+
 /*
   isOdd
   This function takes a number and returns true or false
   based on whether the number is odd or not
 */
+
+const isOdd = (number) => !isEven(number);
 
 /*
   square
@@ -16,17 +20,22 @@
   of that number
 */
 
+const square = (number) => number * number;
+
 /*
   cube
   This function takes a number and returns the mathematical cube
   of that number
 */
 
+const cube = (number) => square(number) * number;
 
 /*
   gcd
   This function returns the greatest common divisor of any two numbers
 */
+
+const gcd = (a,b) => (a * b) / lcm(a, b)
 
 
 /*
@@ -34,6 +43,15 @@
   This function returns the least common multiple of any two numbers
 */
 
+const leastOfTwo = (a, b) => a < b ? a : b;
+
+const isDivisibleBy = (number , divisor) => number % divisor == 0
+
+const lcm = function(a, b) {
+  let multiple = leastOfTwo(a, b);
+  while(!isDivisibleBy(multiple, a) || !isDivisibleBy(multiple, b)) multiple++;
+  return multiple;
+}
 
 
 /*
@@ -42,6 +60,7 @@
   principle, period and rate of interest(in that order)
 */
 
+const simpleInterest = (principle, time, rate) => (principle * rate * time) / 100;
 
 /*
   compoundInterest
@@ -49,17 +68,31 @@
   principle, period and rate of interest annually(in that order)
 */
 
+const compoundInterest = function(principle, time, rate) {
+  let amount = principle;
+  while(time > 0) {
+    amount = amount + simpleInterest(amount, 1, rate);
+    time--;
+  }
+  return amount - principle;
+}
+
 
 /*
   greatestOf
   This function returns the greatest of three numbers
 */
 
+const greatestOfTwo = (a, b) => a > b ? a : b;
+
+const greatestOf = (a, b, c) => greaterOfTwo(c, greaterOfTwo(a, b));
 
 /*
   averageOf
   This function returns the average of three numbers
 */
+
+const averageOf = (a, b, c) => (a + b + c) / 3;
 
 
 exports.isEven=isEven;
